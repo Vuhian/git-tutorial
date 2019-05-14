97c41b77d44202d7c717939fc5651c90c324d694
Isaac Dinis <isaac.dinis@epfl.ch>
2019-05-14 16:39:28 +0200

    updated main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+#include "espl_lib.h"
 
 int main() {
-	printf("Hello World\n");
+	int n = 0, dummy = 1;
+	while (dummy){
+	
+		printf("choose a nunber \n");
+		scanf("%d",&n);
+		printf("%s \n",num_to_words(n));
+		printf("press 1 to continue or 0 to stop \n");
+		scanf("%d", &dummy);
+	}	
 	return 0;
 }
